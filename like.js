b5dc4301d67b9fc269988adb3244e3c4f77fb732
Dennis Ig <denisigoshev@gmail.com>
2019-12-11 11:43:23 +0000

    Update like.js

--- like.js
+++ like.js
@@ -1,9 +1,11 @@
-var confirmBox = confirm('Start swiping right?');
+var confirmBox = confirm('Start right swiper ?');
 var count = 1;
 var like = function(){
     if (confirmBox){
       document.querySelector('[aria-label="Like"]').click();
-      console.log('Liked ' + count);						  count++;												};
-    setTimeout(like, 100);
+      console.log('Liked ' + count);						  
+        count++;
+    };
+    setTimeout(like, 500);
 };
-setTimeout(like, 100);
+setTimeout(like, 500);
